53bfe9fe5671537816d447ad28e7b51e385137dd
Akshar <mistry.aks@gmail.com>
2021-11-23 20:26:05 +0530

    init dist

---
+++ webpack/dist/main.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+document.getElementById('root').innerHTML='<h1>Loaded</h1>';
+/******/ })()
+;
+//# sourceMappingURL=main.js.map
---
+++ webpack/src/index.js
@@ -0,0 +1,1 @@
+document.getElementById('root').innerHTML='<h1>Loaded</h1>';
